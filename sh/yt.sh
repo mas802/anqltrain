d4c64ac457ae4907bffaa5cb1e08cf655e0dee79
Markus Schaffner <6946570+mas802@users.noreply.github.com>
2025-11-16 19:50:28 +0100

    yt support script

---
+++ sh/yt.sh
@@ -0,0 +1,1 @@
+ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -f v4l2 -i /dev/video0 -c:v libx264 -pix_fmt yuv420p -preset ultrafast -g 10 -b:v 1600k -bufsize 512k -acodec libmp3lame -ar 44100 -threads 2 -qscale 3 -b:a 128k -r 5 -s 640x480 -f flv rtmp://a.rtmp.youtube.com/live2/$1
